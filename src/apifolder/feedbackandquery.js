66d32f606fbaf4160ec58f25d9c6db7825521d75
hemanth260399 <hemanthraja26@gmail.com>
2025-02-04 20:52:45 +0530

    changes

--- src/apifolder/feedbackandquery.js
+++ src/apifolder/feedbackandquery.js
@@ -3,20 +3,12 @@ let url = import.meta.env.VITE_BE_URL
 export let addfeedbackandqueryapi = async (data) => {
     try {
         let response = await axios.post(`${url}/addfeedback`, data,
-<<<<<<< HEAD
             {
                 headers: {
                     "auth-token": localStorage.getItem("token") || "",
                 },
             }
         )
-=======
-                                       {
-                headers: {
-                    "auth-token": localStorage.getItem("token") || "",
-                },
-            })
->>>>>>> e02ef40abc3111562b6736db37f8f4d6e6528a50
         return response.data
     } catch (err) {
         throw new Error(err.response.data.msg)
