2428096e6b68a58504315ce04ba8977af59b104b
hemanth260399 <hemanthraja26@gmail.com>
2025-02-04 23:35:40 +0530

    loading added

--- src/pages/googleLogin.jsx
+++ src/pages/googleLogin.jsx
@@ -1,27 +1,32 @@
-import { useContext, useEffect } from "react"
+import { useContext, useEffect, useState } from "react"
 import { googleLoginApi } from "../apifolder/registerapi"
 import { useNavigate } from "react-router-dom"
 import AppContext from "./Appcontext"
 
 export let LoginSuccess = () => {
     let navigate = useNavigate()
+    let [loading, setloading] = useState(false)
     let { dispatch } = useContext(AppContext)
     useEffect(() => {
         let getUsers = async () => {
-            console.log("hello")
             try {
+                setloading(true)
                 let data = await googleLoginApi()
+                setloading(false)
                 localStorage.setItem("useremail", JSON.stringify(data.user.userdata))
                 localStorage.setItem("token", data.token)
                 dispatch({ type: "LOGIN", payload: data.user.userdata })
                 navigate("/userpage")
             } catch (e) {
+                setloading(false)
                 console.log(e)
             }
         }
         getUsers()
     }, [])
     return (
-        <></>
+        <>
+            {loading && <Loader />
+            }</>
     )
 }
